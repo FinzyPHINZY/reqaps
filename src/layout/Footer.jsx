5c372ab6e3826231edf82be61b2f89805b46d9b4
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-12-04 15:30:03 +0100

    updated project

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,6 +1,6 @@
-import { Link } from "react-router-dom";
-import routesConstants from "../constants/routeConstants";
-import { Icons } from "../assets/icons";
+import { Link } from 'react-router-dom';
+import routesConstants from '../constants/routeConstants';
+import { Icons } from '../assets/icons';
 
 const Footer = () => {
   return (
@@ -9,10 +9,26 @@ const Footer = () => {
         <Link to={routesConstants.HOME}>
           <img src={Icons.logo} className="lg:max-w-[160px] max-w-[120px]" />
         </Link>
+        <p className="text-white">
+          All rights reserved. &copy;
+          <a href="https://finzyphinzy.vercel.app" className="underline">
+            {' '}
+            finzyphinzy
+          </a>
+        </p>
         <div className="flex items-center lg:gap-x-7 md:gap-x-6 gap-x-5">
-          <Link to = "" className="text-base font-semibold text-white hover:text-risd-blue transition-all duration-300 ease-in-out">
-          Already a member?</Link>
-          <Link to = "" className="capitalize text-base font-semibold text-white transition-all duration-300 ease-in-out px-[30px] lg:min-h-[44px] min-h-[40px] border-[1px] border-white border-solid rounded-lg inline-flex items-center justify-center text-center hover:bg-grad-theme-135">log in</Link>
+          <Link
+            to=""
+            className="text-base font-semibold text-white hover:text-risd-blue transition-all duration-300 ease-in-out"
+          >
+            Already a member?
+          </Link>
+          <Link
+            to=""
+            className="capitalize text-base font-semibold text-white transition-all duration-300 ease-in-out px-[30px] lg:min-h-[44px] min-h-[40px] border-[1px] border-white border-solid rounded-lg inline-flex items-center justify-center text-center hover:bg-grad-theme-135"
+          >
+            log in
+          </Link>
         </div>
       </div>
     </footer>
